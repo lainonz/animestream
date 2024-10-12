f7380f402ce08a8585121da932960d94d3f1a006
Herlangga <herlangga.maulani@gmail.com>
2024-10-12 14:32:44 +0700

    zxz

--- src/pages/stream/StreamAnime.jsx
+++ src/pages/stream/StreamAnime.jsx
@@ -6,16 +6,14 @@ import { Link, useParams } from "react-router-dom";
 const StreamAnime = () => {
   const [stream, setStream] = useState(null);
   const [loading, setLoading] = useState(true);
-  const { epsid } = useParams(); // Dapatkan slug dari URL
+  const { epsid } = useParams();
   const [openAccordion, setOpenAccordion] = useState(null);
 
-  // Fungsi untuk toggle accordion
   const toggleAccordion = (index) => {
     setOpenAccordion(openAccordion === index ? null : index);
   };
 
   useEffect(() => {
-    // Mendapatkan data episode
     axios
       .get(`${import.meta.env.VITE_API_URL}/api/anime/episode/${epsid}`)
       .then((res) => {
@@ -25,45 +23,41 @@ const StreamAnime = () => {
       });
   }, [epsid]);
 
-  const handleVideoLoad = async () => {
-    // Coba ambil video setelah interaksi
-    const response = await axios.get(stream.stream_url);
-    // Ambil URL video dari response di sini
-    const videoUrl = response.data.video_url; // Ganti dengan path yang sesuai
-
-    // Lakukan sesuatu dengan videoUrl
-    console.log("Video URL after interaction:", videoUrl);
+  const renderSkeleton = () => {
+    return (
+      <div className="animate-pulse w-full h-[300px] md:h-[600px] bg-gray-300 rounded-md"></div>
+    );
   };
 
   return (
     <>
       <section className="mt-4">
         {loading ? (
-          <p>Loading...</p>
+          renderSkeleton() // Menampilkan skeleton saat loading
         ) : (
           <div>
             <h1 className="mb-2">{stream.episode}</h1>
             <div>
               <iframe
                 src={stream.stream_url}
-                frameborder="0"
-                allow="fullscreen" // Tambahkan ini untuk mengizinkan fullscreen
+                frameBorder="0"
+                allow="fullscreen"
                 sandbox="allow-same-origin allow-scripts"
-                className="w-full h-[300px]  md:h-[600px]"
+                className="w-full h-[300px] md:h-[600px]"
               ></iframe>
             </div>
             <div className="flex mt-3 justify-between gap-2">
-              {stream.prev_episode_slug && ( // Tampilkan link sebelumnya hanya jika slug ada
+              {stream.prev_episode_slug && (
                 <Link
-                  to={`/${stream.anime.slug}/${stream.prev_episode_slug}`} // Ganti path sesuai dengan epsId
+                  to={`/${stream.anime.slug}/${stream.prev_episode_slug}`}
                   className="w-full bg-blue-500 bg-opacity-50 p-2 flex justify-center"
                 >
                   <ChevronLeft size={28} />
                 </Link>
               )}
-              {stream.next_episode_slug && ( // Tampilkan link selanjutnya hanya jika slug ada
+              {stream.next_episode_slug && (
                 <Link
-                  to={`/${stream.anime.slug}/${stream.next_episode_slug}`} // Ganti path sesuai dengan epsId
+                  to={`/${stream.anime.slug}/${stream.next_episode_slug}`}
                   className="w-full bg-blue-500 bg-opacity-50 p-2 flex justify-center"
                 >
                   <ChevronRight size={28} />
